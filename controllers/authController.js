eb591d0851dd446a05f9630df16879f593073761
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-04-01 05:48:57 +0200

    commits

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import generateTfId from "../utils/generateTfId.js";
+import jwt from "jsonwebtoken";
+import sendEmail from "../utils/email.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -18,6 +20,7 @@ export const registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         // Generate TF ID
+        // Generate TF ID
         const tfId = generateTfId();
 
         // Create user
@@ -28,9 +31,61 @@ export const registerUser = async (req, res) => {
             tfId,
         });
 
+        // 🔥 SEND EMAIL
+        const message = `
+            <h2>Welcome to TalentFlow 🚀</h2>
+            <p>Hello ${name},</p>
+            <p>Your account has been created successfully.</p>
+            <p><strong>Your TalentFlow ID:</strong> ${tfId}</p>
+            <p>Keep this ID safe for verification and tracking.</p>
+        `;
+
+        await sendEmail(email, "Welcome to TalentFlow", message);
         res.status(201).json({
             message: "User registered successfully",
             tfId: user.tfId,
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // Generate token
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+                tfId: user.tfId,
+                role: user.role,
+            },
         });
 
     } catch (error) {
