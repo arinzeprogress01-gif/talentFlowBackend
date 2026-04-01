0b81a54379627a8c06400b6c459f8b34c1366770
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-04-01 04:50:12 +0200

    c

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server is runing on PORT ${PORT}`)
-}); 
+if (typeof process.env.PORT !== "undefined") {
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+} else {
+    const PORT = 8080;
+    app.listen(PORT, () => {
+        console,log(`Server running on port ${PORT}`);
+    });
+}
