a3f4be1c306dde57ecbd3546f7a320c350f33447
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-04-05 00:05:23 +0200

    EM

--- utils/email.js
+++ utils/email.js
@@ -3,11 +3,24 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            connectionTimeout: 10000,
+            greetingTimeout: 10000,
+            socketTimeout: 10000,
+         });
+
+         transporter.verify((error, success) => {
+            if (error) {
+                console.log("SMTP ERROR:", error);
+            } else {
+                console.log("SMTP READY");
+            }
         });
 
         const mailOptions = {
