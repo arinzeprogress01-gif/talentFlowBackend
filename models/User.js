09c6754780ebb846adcbdaaf4673215c1056c4ed
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-04-09 11:22:50 +0200

    fix

--- models/User.js
+++ models/User.js
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema(
 
         role: {
             type: String,
-            enum: ["learner", "tutor"],
+            enum: ["learner", "tutor", "admin"],
             default: null,
         },
 
